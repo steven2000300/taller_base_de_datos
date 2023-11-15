e156d2913104ac86281dbdd50e71cb4ff3ee2c1c
steven2000300 <150410789+steven2000300@users.noreply.github.com>
2023-11-15 00:03:39 -0500

    se agrega clases para conexion con MYSQL

--- dist/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Entidad"},{"l":"Graficas"},{"l":"Listados"}];updateSearchResults();
---
+++ src/Entidad/DatosConexion.java
@@ -0,0 +1,9 @@
+package Entidad;
+
+
+public interface DatosConexion {
+    String baseDatos = "sira";
+    String user = "root";
+    String login = "";
+    String host = "127.0.0.1";
+}
